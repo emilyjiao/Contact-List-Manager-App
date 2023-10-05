c10ca1765b63dc4c59f51859c8388c869a52218b
Emily Jiao <emilyjiao@Emilys-MacBook-Air.local>
2023-10-04 22:58:03 -0400

    test push

--- src/main/java/com/assessment/contactlistmanager/ContactListManagerApplication.java
+++ src/main/java/com/assessment/contactlistmanager/ContactListManagerApplication.java
@@ -1,5 +1,5 @@
 package com.assessment.contactlistmanager;
-
+//test push
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +8,7 @@ public class ContactListManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContactListManagerApplication.class, args);
+		System.out.println("rest");
 	}
-
+	
 }
